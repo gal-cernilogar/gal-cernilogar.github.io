2e05905b1313a551d09da93cad20e24954171158
Gal Černilogar <gal.cernilogar@outfit7.com>
2023-04-15 16:12:51 +0200

    Add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+    }
+  },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss', 'nuxt-icon'
   ]
 })
