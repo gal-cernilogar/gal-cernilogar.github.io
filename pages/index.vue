1144f509d56f60cc801fac8a9d268e11c9a6b0e2
Gal Černilogar <gal.cernilogar@outfit7.com>
2023-04-16 22:45:11 +0200

    Change title

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="home" class="place-content-center flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-semibold text-center">Gal
-      Černilogar</h1>
+    <h1 class="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold text-center">GAL</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-
-</script> 
+definePageMeta({
+  title: 'Gal'
+})
+</script>
